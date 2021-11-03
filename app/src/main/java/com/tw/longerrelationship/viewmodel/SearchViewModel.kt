e446796da4d9a0838da73c93b0ce3eb6952592d1
谭炜 <1561974785@qq.com>
2021-11-04 00:06:04 +0800

    commit

--- app/src/main/java/com/tw/longerrelationship/viewmodel/SearchViewModel.kt
+++ app/src/main/java/com/tw/longerrelationship/viewmodel/SearchViewModel.kt
@@ -1,6 +1,5 @@
 package com.tw.longerrelationship.viewmodel
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.PagingData
@@ -8,12 +7,13 @@ import androidx.paging.cachedIn
 import com.tw.longerrelationship.logic.model.DairyItem
 import com.tw.longerrelationship.logic.repository.MainRepository
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 
 class SearchViewModel(val repository: MainRepository) : ViewModel() {
 
 
     fun getKeyDairy(key: String): Flow<PagingData<DairyItem>> {
-        Log.d("www", "请求数据=$key")
+        if (key.isEmpty()) return flow { PagingData.empty<DairyItem>() }
         return repository.getKeyDairyData(key).cachedIn(viewModelScope)
     }
 }
